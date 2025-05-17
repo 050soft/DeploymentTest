095ce2d83bf5e02c78b7d1fb3e5a5de5ca62131c
Not_H3 <67106546+damger9@users.noreply.github.com>
2025-05-17 18:55:05 +0200

    Update test.client.lua

--- src/client/StarterPlayerScripts/test.client.lua
+++ src/client/StarterPlayerScripts/test.client.lua
@@ -3,7 +3,7 @@ print("test2");
 
 print("a");
 
-local version = require(game.ReplicatedStorage:WaitForChild("Version", 20));
+local version = require(game.ReplicatedStorage:WaitForChild("version", 20));
 print(version.version)
 print(version.commit)
 print(version.branch)
